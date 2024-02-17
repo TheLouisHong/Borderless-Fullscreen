8cc14ca28003390d01ec38728c063363dfeb877e
Louis Hong <honglouis97@gmail.com>
2024-02-17 00:28:41 -0800

    Functional commit.

---
+++ src/main/java/com/borderlessfullscreen/BorderlessFullscreenConfig.java
@@ -0,0 +1,24 @@
+package com.borderlessfullscreen;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.Keybind;
+
+
+@ConfigGroup("BorderlessFullScreen")
+public interface BorderlessFullscreenConfig extends Config
+{
+
+    @ConfigItem(
+        keyName = "BorderlessFullscreenHotkey",
+        name = "Fullscreen Hotkey",
+        description = "Hotkey to toggle fullscreen mode",
+        position = 1,
+        section = "Hotkeys"
+    )
+    default Keybind FullScreenHotKey() {
+        return Keybind.NOT_SET;
+    }
+
+}
